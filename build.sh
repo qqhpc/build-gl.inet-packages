e6deb400d34e1422c48bb8e47cdaef573510a85f
qqhpc <43428900+qqhpc@users.noreply.github.com>
2022-08-03 15:33:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo SOURCECODEURL: "$SOURCECODEURL"
 echo PKGNAME: "$PKGNAME"
 echo BOARD: "$BOARD"
-EMAIL=${EMAIL:-"aa@163.com"}
+EMAIL=${EMAIL:-"github-actions[bot]@github.com"}
 echo EMAIL: "$EMAIL"
 echo PASSWORD: "$PASSWORD"
 
@@ -12,7 +12,7 @@ sudo -E apt-get update
 sudo -E apt-get install git  asciidoc bash bc binutils bzip2 fastjar flex gawk gcc genisoimage gettext git intltool jikespg libgtk2.0-dev libncurses5-dev libssl1.0-dev make mercurial patch perl-modules python2.7-dev rsync ruby sdcc subversion unzip util-linux wget xsltproc zlib1g-dev zlib1g-dev -y
 
 git config --global user.email "${EMAIL}"
-git config --global user.name "aa"
+git config --global user.name "github-actions[bot]"
 [ -n "${PASSWORD}" ] && git config --global user.password "${PASSWORD}"
 
 mkdir -p  ${WORKDIR}/buildsource
@@ -23,14 +23,13 @@ cd  ${WORKDIR}
 
 mips_siflower_sdk_get()
 {
-	 git clone https://github.com/gl-inet-builder/openwrt-sdk-siflower-1806.git openwrt-sdk
+	 git clone https://github.com/qqhpc/openwrt-sdk-siflower-1806.git openwrt-sdk
 }
 
 axt1800_sdk_get()
 {
-	wget -q -O openwrt-sdk.tar.xz https://fw.gl-inet.com/releases/v21.02-SNAPSHOT/sdk/openwrt-sdk-ipq807x-ipq60xx_gcc-5.5.0_musl_eabi.Linux-x86_64.tar.xz
 	mkdir -p ${WORKDIR}/openwrt-sdk
-	tar -Jxf openwrt-sdk.tar.xz -C ${WORKDIR}/openwrt-sdk --strip=1
+	git clone https://github.com/qqhpc/openwrt-sdk.git ${WORKDIR}/openwrt-sdk
 	echo src-git packages https://git.openwrt.org/feed/packages.git^78bcd00c13587571b5c79ed2fc3363aa674aaef7 >${WORKDIR}/openwrt-sdk/feeds.conf.default
 	echo src-git routing https://git.openwrt.org/feed/routing.git^a0d61bddb3ce4ca54bd76af86c28f58feb6cc044 >>${WORKDIR}/openwrt-sdk/feeds.conf.default
 	echo src-git telephony https://git.openwrt.org/feed/telephony.git^0183c1adda0e7581698b0ea4bff7c08379acf447 >>${WORKDIR}/openwrt-sdk/feeds.conf.default
